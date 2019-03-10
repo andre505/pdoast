0741dfb81a183238a85c4e118490d70d0fe6231e
andre505 <anthony.odu@up-ng.com>
2019-03-10 12:20:55 +0100

    resetpass_

---
+++ app/resetpass/resetpass.js
@@ -0,0 +1,111 @@
+const app = require("tns-core-modules/application");
+const httpModule = require("http");
+var view = require("ui/core/view");
+const LoginViewModel = require("./login-view-model");
+const appSettings = require("application-settings");
+
+function onNavigatingTo(args) {
+    const page = args.object;
+    page.bindingContext = new LoginViewModel();
+    }
+
+function onResetTap(args) {
+    
+    const button = args.object;
+    const page = button.page;
+    var ac = view.getViewById(page, "myIndicator");
+    ac.busy = true;
+    var loginredirectURL ="";  
+    var textfield= view.getViewById(page, "usrtxtfld");
+    var textfield2= view.getViewById(page, "passtxtfld");
+    var txtfldstatus = view.getViewById(page, "loginstatus");
+    var user = textfield.text;
+    var pass = textfield2.text;
+    var usertoken = "";
+    var useridentity = ""; 
+    var myterminalist = [];   
+    
+    //logic for validation
+    httpModule.request({
+        url: "http://172.19.8.170:8484/api/auth/token",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify({
+            Username: user,
+            Password: pass
+        })
+        }).then((response1) => {
+        const result = response1.content.toJSON();
+        //
+        if (result.success){    
+            usertoken = result.status;
+            useridentity = result.name; 
+            
+            const savedusertoken = appSettings.getString(user);
+            if (username == null)
+            {
+            appSettings.setString(user, usertoken);
+            }
+            //var toke = appSettings.getString(user);
+            //Get Terminal List
+            httpModule.getJSON({
+                url: "http://172.19.8.170:8484/api/task/index",
+                method: "GET",
+                headers: {"Content-Type": "application/json","Authorization":"Bearer"+ " "+usertoken}
+            }).then((response) => {               
+            myterminalist = response;
+            const navigationEntry = {
+                moduleName: "terminal/terminal",
+               context: {param1: useridentity,
+                        param2: user,
+                        param3: savedusertoken,
+                        terminalist: myterminalist},
+                        animated: true,
+                        transition: {
+                            name: "slide",
+                            duration: 380,
+                            curve: "easeIn"
+                        },
+                clearHistory: true               
+            };
+            page.frame.navigate(navigationEntry)    
+
+            
+                //response.forEach(x => navigationContext.myterminalist.push(x));    
+            }, (e) => {
+            });  
+            //
+            //==================const navigation entry used to be here
+
+          // page.frame.navigate(navigationEntry)=====================================
+            
+            //frame.navigate(navigationEntry);            
+            //page.frame.navigate(navigationEntry);       
+        }
+        else 
+        {     
+            txtfldstatus.text = "Invalid Login";
+            ac.busy = false;
+            //page.frame.navigate("login/home-page"); 
+            //var signedinpdo = new pdo; --------------formerly before navigation entry
+            //signedinpdo.token = usertoken;
+            //signedinpdo.Username = useridentity;           
+        }
+
+
+    }, (e) => {
+        ac.busy = false;
+        txtfldstatus.text = "A network error occurred";
+        //console.log("Error: ");
+            //console.log(e);
+    });
+    //end validation logic
+
+        
+   // return page;
+    //page.frame.navigate ("\""+loginredirectURL+"\"");
+}
+
+
+exports.onNavigatingTo = onNavigatingTo;
+exports.onResetTap = onResetTap;
